8e6a418a90855c955609b9717973851bcf13200e
keithperez <keithperezprofessional@gmail.com>
2026-08-30 22:16:41 -0400

    Win condition and another thing of text

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,40 +1,62 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using NUnit.Framework.Constraints;
 
 public class ScoreManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private int score;
+    private bool failed = false;
+    private int misses;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI missesText;
     
     void Start()
     {
         score = 0;
-        scoreText.text = "SCORE: " + score;
+        misses = 0;
+        scoreText.text = "Get to 300 score!\nMiss 5 balls, and you lose!";
+        missesText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void addScore(int addedScore)
     {
-        score+=addedScore;
-        scoreText.text = "SCORE: " + score;
+        if (!failed) {
+            score+=addedScore;
+            scoreText.text = "SCORE: " + score;
+
+            if (score >= 300)
+            {
+                missesText.text = "YOU WIN! GO FOR A HIGH SCORE!";
+            }
+
+        }
     }
 
     public void deductScore(int deductedScore)
     {
-        score -= deductedScore;
-        scoreText.text = "SCORE: " + score;
-
-        if (score < -20)
-        {
-            scoreText.text = "YOU LOSE!!";
+        if (!failed) {
+            score -= deductedScore;
+            scoreText.text = "SCORE: " + score;
+            missesText.text = "MISSES: ";
+            misses++;
+            for (int i=0;i < misses;i++) // for every miss on the board already
+            {
+                missesText.text += "X ";
+            }
+            if (misses >= 5) {failed = true;}
         }
+        if (failed)
+        {
+            missesText.text = "YOU LOSE!";
+        }
+
     }
 
     public int getScore()
